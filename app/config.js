757f4263cd6f58ffcc59c016e6d9e02db6b2d972
johnwon <hantig1986@outlook.com>
2019-03-22 22:09:58 +0800

    added build 1

--- app/config.js
+++ app/config.js
@@ -2,10 +2,10 @@ class Config{
     constructor(){
         // this.EXCHANGE_RATE_API_URL="http://api.openrates.io" 
         this.EXCHANGE_RATE_API_URL="https://api.exchangeratesapi.io"
-        this.BACKEND_API_URL="http://localhost:3031"
-        this.BACKEND_FILE_URL="http://localhost:3032/"
-        // this.BACKEND_API_URL="http://159.122.201.34:32003"
-        // this.BACKEND_FILE_URL="http://159.122.201.34:32003"
+        // this.BACKEND_API_URL="http://localhost:3031"
+        // this.BACKEND_FILE_URL="http://localhost:3032/"
+        this.BACKEND_API_URL="http://18.191.175.213:3031"
+        this.BACKEND_FILE_URL="http://18.191.175.213:3031"
     }
 }
 

ad3636b28af4c782b8a921f5ca8bb63e094dd271
johnwon <hantig1986@outlook.com>
2019-03-23 13:23:03 +0800

    fixed slash

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@ class Config{
         // this.BACKEND_API_URL="http://localhost:3031"
         // this.BACKEND_FILE_URL="http://localhost:3032/"
         this.BACKEND_API_URL="http://18.191.175.213:3031"
-        this.BACKEND_FILE_URL="http://18.191.175.213:3031"
+        this.BACKEND_FILE_URL="http://18.191.175.213:3031/"
     }
 }
 
